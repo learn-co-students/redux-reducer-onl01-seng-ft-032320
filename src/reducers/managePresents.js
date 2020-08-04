d8a59268b6cb48274b933866ad0e9553718e81c3
github username <github email address>
2020-08-04 10:14:29 -0400

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,7 +1,7 @@
 export function managePresents(state, action) {
 
-    // state = { numberOfPresents: 0 }
-    // action = { type: "INCREASE" }
+    //numberOfPresents is key inside state, let state ={numberOfPresents:0}
+    // let action = {type: "INCREASE"} this check a condition match when action is dispatch to update state if it match
 
     switch (action.type) {
         case "INCREASE":
